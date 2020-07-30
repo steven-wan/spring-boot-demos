96cbbdcd51f5184dce94f4291053b57b44de3edc
steven.wan <951520698@qq.com>
2020-07-30 09:20:11 +0800

    jpa和mybatis

---
+++ spring-boot-mybatisAndSpringDataJpa/src/main/java/com/example/mybatisAndSpringDataJpa/JpaAndMybatisApplication.java
@@ -0,0 +1,15 @@
+package com.example.mybatisAndSpringDataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAndMybatisApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAndMybatisApplication.class, args);
+
+    }
+
+}
